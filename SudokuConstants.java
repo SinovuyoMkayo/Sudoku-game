30504f47ffceb48d642bed6c63a07e647cae58bd
g22k9432 <125566358+g22k9432@users.noreply.github.com>
2023-10-08 21:50:14 +0200

    Add files via upload

---
+++ SudokuConstants.java
@@ -0,0 +1,9 @@
+package sudoku;
+
+public class SudokuConstants {
+	
+	public static final int BOARD_SIZE = 9;  //size of the sudoku board
+	
+	public static final int SUBBOARD_SIZE = 3; //size of sub grid of the sudoku board
+		
+}
